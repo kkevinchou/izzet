f1ece5238f22a0499c7eb14ab0502b45b18b0905
kkevinchou <kkevinchou@hotmail.com>
2023-01-16 03:14:40 -0500

    rotation gizmo visual update

--- shaders/unit_circle.fs
+++ shaders/unit_circle.fs
@@ -8,11 +8,15 @@ void main() {
     // place the origin of our local coordinate system to be the middle of the rendered area
     vec2 uv = gl_FragCoord.xy / vec2(1024, 1024) * 2 - 1;
 
-    float distance = 1 - length(uv);
-    distance = step(0, distance);
-    if (distance < 0.1) {
+    // float distance = 1 - length(uv);
+    // float stepDistance = step(0, distance);
+    // if (distance > 0.3 || stepDistance < 0.1) {
+
+    float dist = length(uv);
+
+    if (dist > 1 || dist < 0.9) {
         discard;
     } else {
-        FragColor = vec4(vec3(color), distance * color.a);
+        FragColor = vec4(vec3(color), color.a);
     }
 }

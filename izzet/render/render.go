d326a05b4b8ac309912ca71774b251425267bb3f
kkevinchou <kkevinchou@hotmail.com>
2023-02-08 02:59:09 -0500

    use scale when color picking

--- izzet/render/render.go
+++ izzet/render/render.go
@@ -404,7 +404,7 @@ func (r *Renderer) renderColorPicking(viewerContext ViewerContext) {
 
 	for _, entity := range r.world.Entities() {
 		modelMatrix := createModelMatrix(
-			mgl64.Scale3D(1, 1, 1),
+			mgl64.Scale3D(entity.Scale.X(), entity.Scale.Y(), entity.Scale.Z()),
 			entity.Rotation.Mat4(),
 			mgl64.Translate3D(entity.Position[0], entity.Position[1], entity.Position[2]),
 		)

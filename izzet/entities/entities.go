48f8f4044c9cf7d08cc090f6c6f86719ab6c113d
kkevinchou <kkevinchou@hotmail.com>
2023-02-11 16:17:34 -0500

    remove alpha scale

--- izzet/entities/entities.go
+++ izzet/entities/entities.go
@@ -48,7 +48,7 @@ func InstantiateFromPrefabStaticID(id int, prefab *prefabs.Prefab) *Entity {
 	if len(prefab.ModelRefs[0].Model.Animations()) > 0 {
 		e.AnimationPlayer = animation.NewAnimationPlayer(prefab.ModelRefs[0].Model)
 		e.AnimationPlayer.PlayAnimation("Walk")
-		e.Scale = mgl64.Vec3{10, 10, 10}
+		// e.Scale = mgl64.Vec3{10, 10, 10}
 	}
 
 	return e

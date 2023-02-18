d7e66fd2eca019bdd1c861950b77656395fa11d5
kkevinchou <kkevinchou@hotmail.com>
2023-02-18 17:48:22 -0500

    support scaling all axes at once

--- izzet/gizmo/scale.go
+++ izzet/gizmo/scale.go
@@ -4,17 +4,31 @@ import "github.com/go-gl/mathgl/mgl64"
 
 var S *ScaleGizmo
 
+type AxisType int
+
+const (
+	NullAxis AxisType = iota
+	AllAxis
+	XAxis
+	YAxis
+	ZAxis
+)
+
+type Axis struct {
+	Vector mgl64.Vec3
+	Type   AxisType
+}
+
 type ScaleGizmo struct {
-	ScaleDir        mgl64.Vec3
 	MotionPivot     mgl64.Vec2
 	Active          bool
-	Axes            []mgl64.Vec3
-	HoverIndex      int
+	Axes            []Axis
+	HoveredAxisType AxisType
 	ActivationScale mgl64.Vec3
 }
 
 func (g *ScaleGizmo) Reset() {
-	g.HoverIndex = -1
+	g.HoveredAxisType = NullAxis
 	g.ActivationScale = mgl64.Vec3{}
 	g.Active = false
 }

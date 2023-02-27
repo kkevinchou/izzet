fde561050d18bdfeda239ed4977bfc1c575817b0
kkevinchou <kkevinchou@hotmail.com>
2023-02-27 03:34:21 -0500

    moving code around

--- izzet/render/render.go
+++ izzet/render/render.go
@@ -1,14 +1,11 @@
 package render
 
 import (
-	"encoding/binary"
-	"errors"
 	"fmt"
 	"math"
 	"time"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
-	"github.com/go-gl/mathgl/mgl32"
 	"github.com/go-gl/mathgl/mgl64"
 	"github.com/inkyblackness/imgui-go/v4"
 	"github.com/kkevinchou/izzet/izzet/camera"
@@ -197,7 +194,7 @@ func (r *Renderer) Render(delta time.Duration) {
 
 	r.renderToSquareDepthMap(lightViewerContext, lightContext)
 	r.renderToCubeDepthMap(lightContext)
-	r.renderColorPicking(cameraViewerContext)
+	r.renderToColorPickingBuffer(cameraViewerContext)
 	r.renderToDisplay(cameraViewerContext, lightContext)
 
 	r.renderGizmos(cameraViewerContext)
@@ -427,7 +424,7 @@ func (r *Renderer) renderScene(viewerContext ViewerContext, lightContext LightCo
 	}
 }
 
-func (r *Renderer) renderColorPicking(viewerContext ViewerContext) {
+func (r *Renderer) renderToColorPickingBuffer(viewerContext ViewerContext) {
 	defer resetGLRenderSettings()
 	w, h := r.world.Window().GetSize()
 	gl.Viewport(0, 0, int32(w), int32(h))
@@ -501,13 +498,6 @@ func (r *Renderer) renderImgui() {
 	imgui.PushStyleVarVec2(imgui.StyleVarFramePadding, imgui.Vec2{})
 	imgui.PushStyleColor(imgui.StyleColorText, imgui.Vec4{X: .65, Y: .79, Z: 0.30, W: 1})
 
-	// var open1 bool
-	// imgui.SetNextWindowBgAlpha(0)
-	// imgui.SetNextWindowPosV(imgui.Vec2{}, imgui.ConditionAlways, imgui.Vec2{})
-	// imgui.SetNextWindowSizeV(imgui.Vec2{X: float32(settings.Width), Y: float32(settings.Height)}, imgui.ConditionAlways)
-	// imgui.BeginV("explorer root", &open1, imgui.WindowFlagsNoTitleBar|imgui.WindowFlagsNoMove|imgui.WindowFlagsNoCollapse|imgui.WindowFlagsNoResize|imgui.WindowFlagsMenuBar)
-	// imgui.MenuItem("test")
-
 	panels.BuildExplorer(r.world.Entities(), r.world, menuBarSize)
 	panels.BuildPrefabs(r.world.Prefabs(), r.world)
 	panels.BuildDebug(
@@ -515,8 +505,6 @@ func (r *Renderer) renderImgui() {
 		r.depthCubeMapTexture,
 		r.aspectRatio,
 	)
-
-	// imgui.End()
 
 	imgui.PopStyleColor()
 	imgui.PopStyleVarV(10)
@@ -532,32 +520,6 @@ func (r *Renderer) renderImgui() {
 
 	imgui.Render()
 	r.imguiRenderer.Render(r.world.Platform().DisplaySize(), r.world.Platform().FramebufferSize(), imgui.RenderedDrawData())
-}
-
-func (r *Renderer) renderCircle() {
-	defer gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
-	shaderManager := r.shaderManager
-	var alpha float64 = 1
-
-	gl.BindFramebuffer(gl.FRAMEBUFFER, r.redCircleFB)
-	gl.ClearColor(0, 0.5, 0, 0)
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-	drawCircle(shaderManager.GetShaderProgram("unit_circle"), mgl64.Vec4{1, 0, 0, alpha})
-
-	gl.BindFramebuffer(gl.FRAMEBUFFER, r.greenCircleFB)
-	gl.ClearColor(0, 0.5, 0, 0)
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-	drawCircle(shaderManager.GetShaderProgram("unit_circle"), mgl64.Vec4{0, 1, 0, alpha})
-
-	gl.BindFramebuffer(gl.FRAMEBUFFER, r.blueCircleFB)
-	gl.ClearColor(0, 0.5, 0, 0)
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-	drawCircle(shaderManager.GetShaderProgram("unit_circle"), mgl64.Vec4{0, 0, 1, alpha})
-
-	gl.BindFramebuffer(gl.FRAMEBUFFER, r.yellowCircleFB)
-	gl.ClearColor(0, 0.5, 0, 0)
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-	drawCircle(shaderManager.GetShaderProgram("unit_circle"), mgl64.Vec4{1, 1, 0, alpha})
 }
 
 func (r *Renderer) renderGizmos(viewerContext ViewerContext) {
@@ -585,218 +547,4 @@ func (r *Renderer) renderToDisplay(viewerContext ViewerContext, lightContext Lig
 	gl.Viewport(0, 0, int32(w), int32(h))
 	gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
 	r.renderScene(viewerContext, lightContext)
-}
-
-func createModelMatrix(scaleMatrix, rotationMatrix, translationMatrix mgl64.Mat4) mgl64.Mat4 {
-	return translationMatrix.Mul4(rotationMatrix).Mul4(scaleMatrix)
-}
-
-func drawTranslationGizmo(viewerContext *ViewerContext, shader *shaders.ShaderProgram, position mgl64.Vec3) {
-	colors := []mgl64.Vec3{mgl64.Vec3{1, 0, 0}, mgl64.Vec3{0, 0, 1}, mgl64.Vec3{0, 1, 0}}
-
-	for i, axis := range gizmo.T.Axes {
-		lines := [][]mgl64.Vec3{
-			[]mgl64.Vec3{position, position.Add(axis)},
-		}
-		color := colors[i]
-		if i == gizmo.T.HoverIndex {
-			color = mgl64.Vec3{1, 1, 0}
-		}
-		drawLines(*viewerContext, shader, lines, 1, color)
-	}
-}
-
-func drawScaleGizmo(viewerContext *ViewerContext, shader *shaders.ShaderProgram, position mgl64.Vec3) {
-	axisColors := map[gizmo.AxisType]mgl64.Vec3{
-		gizmo.XAxis: mgl64.Vec3{1, 0, 0},
-		gizmo.YAxis: mgl64.Vec3{0, 0, 1},
-		gizmo.ZAxis: mgl64.Vec3{0, 1, 0},
-	}
-	var cubeSize float64 = 5
-	cubeLineThickness := 0.5
-	hoverColor := mgl64.Vec3{1, 1, 0}
-
-	for _, axis := range gizmo.S.Axes {
-		lines := [][]mgl64.Vec3{
-			[]mgl64.Vec3{position, position.Add(axis.Vector)},
-		}
-		color := axisColors[axis.Type]
-		if axis.Type == gizmo.S.HoveredAxisType || gizmo.S.HoveredAxisType == gizmo.AllAxis {
-			color = hoverColor
-		}
-		drawLines(*viewerContext, shader, lines, 1, color)
-
-		cLines := cubeLines(cubeSize)
-		for _, line := range cLines {
-			for i := range line {
-				line[i] = line[i].Add(position).Add(axis.Vector)
-			}
-		}
-		drawLines(*viewerContext, shader, cLines, cubeLineThickness, color)
-	}
-
-	// center of scale gizmo
-	cLines := cubeLines(cubeSize)
-	for _, line := range cLines {
-		for i := range line {
-			line[i] = line[i].Add(position)
-		}
-	}
-	var cubeColor = mgl64.Vec3{1, 1, 1}
-	if gizmo.S.HoveredAxisType == gizmo.AllAxis {
-		cubeColor = hoverColor
-	}
-	drawLines(*viewerContext, shader, cLines, cubeLineThickness, cubeColor)
-}
-func (r *Renderer) drawCircleGizmo(cameraViewerContext *ViewerContext, position mgl64.Vec3) {
-	defer resetGLRenderSettings()
-	w, h := r.world.Window().GetSize()
-	gl.Viewport(0, 0, int32(w), int32(h))
-
-	t := mgl32.Translate3D(float32(position[0]), float32(position[1]), float32(position[2]))
-	s := mgl32.Scale3D(25, 25, 25)
-
-	rotations := []mgl32.Mat4{
-		mgl32.Ident4(),
-		mgl32.HomogRotate3DY(90 * math.Pi / 180),
-		mgl32.HomogRotate3DX(-90 * math.Pi / 180),
-	}
-
-	textures := []uint32{r.redCircleTexture, r.greenCircleTexture, r.blueCircleTexture}
-
-	r.renderCircle()
-	for i := 0; i < 3; i++ {
-		modelMatrix := t.Mul4(rotations[i]).Mul4(s)
-		texture := textures[i]
-		if i == gizmo.R.HoverIndex {
-			texture = r.yellowCircleTexture
-		}
-		drawTexturedQuad(cameraViewerContext, r.shaderManager, texture, 1, float32(r.aspectRatio), &modelMatrix, true)
-	}
-}
-
-func (r *Renderer) initFrameBuffer(width int, height int) (uint32, uint32) {
-	var fbo uint32
-	gl.GenFramebuffers(1, &fbo)
-	gl.BindFramebuffer(gl.FRAMEBUFFER, fbo)
-	defer gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
-
-	var texture uint32
-	gl.GenTextures(1, &texture)
-	gl.BindTexture(gl.TEXTURE_2D, texture)
-	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
-		int32(width), int32(height), 0, gl.RGBA, gl.UNSIGNED_BYTE, nil)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-
-	gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
-
-	var rbo uint32
-	gl.GenRenderbuffers(1, &rbo)
-	gl.BindRenderbuffer(gl.RENDERBUFFER, rbo)
-	gl.RenderbufferStorage(gl.RENDERBUFFER, gl.DEPTH24_STENCIL8, int32(width), int32(height))
-	gl.BindRenderbuffer(gl.RENDERBUFFER, 0)
-
-	gl.FramebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, rbo)
-	if gl.CheckFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE {
-		panic(errors.New("failed to initalize frame buffer"))
-	}
-
-	return fbo, texture
-}
-
-func (r *Renderer) clearMainFrameBuffer() {
-	gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-}
-
-func (r *Renderer) renderSkybox() {
-	defer resetGLRenderSettings()
-	w, h := r.world.Window().GetSize()
-	gl.Viewport(0, 0, int32(w), int32(h))
-
-	drawWithNDC(r.shaderManager)
-}
-
-func (r *Renderer) ViewerContext() ViewerContext {
-	return r.viewerContext
-}
-
-func (r *Renderer) handleResize() {
-	w, h := r.world.Window().GetSize()
-	r.aspectRatio = float64(w) / float64(h)
-	r.fovY = mgl64.RadToDeg(2 * math.Atan(math.Tan(mgl64.DegToRad(fovx)/2)/r.aspectRatio))
-}
-
-func (r *Renderer) GetEntityByPixelPosition(pixelPosition mgl64.Vec2) *int {
-	gl.BindFramebuffer(gl.FRAMEBUFFER, r.colorPickingFB)
-	defer gl.BindFramebuffer(gl.FRAMEBUFFER, 0)
-
-	gl.PixelStorei(gl.UNPACK_ALIGNMENT, 1)
-	data := make([]byte, 4)
-	_, h := r.world.Window().GetSize()
-	gl.ReadPixels(int32(pixelPosition[0]), int32(h)-int32(pixelPosition[1]), 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(data))
-
-	// discard the alpha channel data
-	data[3] = 0
-
-	// NOTE(kevin) actually not sure why, but this works
-	// i would've expected to need to multiply by 255, but apparently it's handled somehow
-	uintID := binary.LittleEndian.Uint32(data)
-	if uintID == settings.EmptyColorPickingID {
-		return nil
-	}
-
-	id := int(uintID)
-	return &id
-}
-
-func computeCubeMapTransforms(position mgl64.Vec3, near, far float64) []mgl64.Mat4 {
-	projectionMatrix := mgl64.Perspective(mgl64.DegToRad(90), float64(settings.DepthCubeMapWidth)/float64(settings.DepthCubeMapHeight), near, far)
-
-	cubeMapTransforms := []mgl64.Mat4{
-		projectionMatrix.Mul4( // right
-			mgl64.LookAtV(
-				position,
-				position.Add(mgl64.Vec3{1, 0, 0}),
-				mgl64.Vec3{0, -1, 0},
-			),
-		),
-		projectionMatrix.Mul4( // left
-			mgl64.LookAtV(
-				position,
-				position.Add(mgl64.Vec3{-1, 0, 0}),
-				mgl64.Vec3{0, -1, 0},
-			),
-		),
-		projectionMatrix.Mul4( // up
-			mgl64.LookAtV(
-				position,
-				position.Add(mgl64.Vec3{0, 1, 0}),
-				mgl64.Vec3{0, 0, 1},
-			),
-		),
-		projectionMatrix.Mul4( // down
-			mgl64.LookAtV(
-				position,
-				position.Add(mgl64.Vec3{0, -1, 0}),
-				mgl64.Vec3{0, 0, -1},
-			),
-		),
-		projectionMatrix.Mul4( // back
-			mgl64.LookAtV(
-				position,
-				position.Add(mgl64.Vec3{0, 0, 1}),
-				mgl64.Vec3{0, -1, 0},
-			),
-		),
-		projectionMatrix.Mul4( // front
-			mgl64.LookAtV(
-				position,
-				position.Add(mgl64.Vec3{0, 0, -1}),
-				mgl64.Vec3{0, -1, 0},
-			),
-		),
-	}
-	return cubeMapTransforms
 }

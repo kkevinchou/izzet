8894e9507b077615ed214742ee4141ed0b1e8beb
kkevinchou <kkevinchou@hotmail.com>
2023-03-12 23:12:55 -0400

    ui styling

--- izzet/panels/debug.go
+++ izzet/panels/debug.go
@@ -64,48 +64,65 @@ func BuildDebug(world World, renderContext RenderContext) {
 
 	imgui.BeginV("Debug", &open, imgui.WindowFlagsNone)
 
-	if imgui.CollapsingHeaderV("Lighting Options", imgui.TreeNodeFlagsDefaultOpen) {
-		imgui.SliderFloat("ambient factor", &DBG.AmbientFactor, 0, 1)
-		imgui.Dummy(imgui.Vec2{X: 0, Y: 10})
-		imgui.SliderFloat("point light bias", &DBG.PointLightBias, 0, 1)
-		imgui.InputInt("point light intensity", &DBG.PointLightIntensity)
-		imgui.Dummy(imgui.Vec2{X: 0, Y: 10})
-		imgui.InputInt("directional light intensity", &DBG.DirectionalLightIntensity)
-		imgui.SliderFloat3("directional light dir", &DBG.DirectionalLightDir, -1, 1)
-
-		imgui.Dummy(imgui.Vec2{X: 0, Y: 10})
-		imgui.SliderFloat("bloom intensity", &DBG.BloomIntensity, 0, 1)
-		imgui.SliderInt("bloom threshold passes", &DBG.BloomThresholdPasses, 0, 3)
-		imgui.SliderFloat("bloom threshold", &DBG.BloomThreshold, 0, 3)
-		imgui.SliderFloat("upsampling scale", &DBG.BloomUpsamplingScale, 0, 5.0)
-		imgui.ColorEdit3V("color picker", &DBG.Color, imgui.ColorEditFlagsNoInputs|imgui.ColorEditFlagsNoLabel)
-		imgui.SameLine()
-		imgui.SliderFloat("color intensity", &DBG.ColorIntensity, 0, 50)
-
-		imgui.Checkbox("bloom", &DBG.Bloom)
-		imgui.Checkbox("enable shadow mapping", &DBG.EnableShadowMapping)
+	if imgui.CollapsingHeaderV("Lighting", imgui.TreeNodeFlagsDefaultOpen) {
+		imgui.BeginTableV("Lights", 2, imgui.TableFlagsBordersInnerV, imgui.Vec2{}, 0)
+		setupRow("Ambient Factor", func() { imgui.SliderFloat("", &DBG.AmbientFactor, 0, 1) })
+		setupRow("Point Light Bias", func() { imgui.SliderFloat("", &DBG.PointLightBias, 0, 1) })
+		setupRow("Point Light Intensity", func() { imgui.InputInt("", &DBG.PointLightIntensity) })
+		setupRow("Directional Light Intensity", func() { imgui.InputInt("", &DBG.DirectionalLightIntensity) })
+		setupRow("Directional Light DIrection", func() { imgui.SliderFloat3("", &DBG.DirectionalLightDir, -1, 1) })
+		setupRow("Color", func() { imgui.ColorEdit3V("", &DBG.Color, imgui.ColorEditFlagsNoInputs|imgui.ColorEditFlagsNoLabel) })
+		setupRow("Color Intensity", func() { imgui.SliderFloat("", &DBG.ColorIntensity, 0, 50) })
+		setupRow("Enable Shadow Mapping", func() { imgui.Checkbox("", &DBG.EnableShadowMapping) })
+		imgui.EndTable()
 	}
+	if imgui.CollapsingHeaderV("Bloom", imgui.TreeNodeFlagsDefaultOpen) {
+		imgui.BeginTableV("Bloom Table", 2, imgui.TableFlagsBordersInnerV, imgui.Vec2{}, 0)
+		setupRow("Enable Bloom", func() { imgui.Checkbox("", &DBG.Bloom) })
+		setupRow("Bloom Intensity", func() { imgui.SliderFloat("", &DBG.BloomIntensity, 0, 1) })
+		setupRow("Bloom Threshold Passes", func() { imgui.SliderInt("", &DBG.BloomThresholdPasses, 0, 3) })
+		setupRow("Bloom Threshold", func() { imgui.SliderFloat("", &DBG.BloomThreshold, 0, 3) })
+		setupRow("Upsampling Scale", func() { imgui.SliderFloat("", &DBG.BloomUpsamplingScale, 0, 5.0) })
+		imgui.EndTable()
+	}
+
 	if imgui.CollapsingHeaderV("Other", imgui.TreeNodeFlagsNone) {
-		imgui.SliderFloat("roughness", &DBG.Roughness, 0, 1)
-		imgui.SliderFloat("metallic", &DBG.Metallic, 0, 1)
-		imgui.SliderFloat("exposure", &DBG.Exposure, 0, 1)
-		imgui.Checkbox("material override", &DBG.MaterialOverride)
-		imgui.Checkbox("render spatial partition", &DBG.RenderSpatialPartition)
+		imgui.BeginTableV("Bloom Table", 2, imgui.TableFlagsBordersInnerV, imgui.Vec2{}, 0)
+		setupRow("Roughness", func() { imgui.SliderFloat("", &DBG.Roughness, 0, 1) })
+		setupRow("Metallic", func() { imgui.SliderFloat("", &DBG.Metallic, 0, 1) })
+		setupRow("Exposure", func() { imgui.SliderFloat("", &DBG.Exposure, 0, 1) })
+		setupRow("Material Override", func() { imgui.Checkbox("", &DBG.MaterialOverride) })
+		setupRow("Render Spatial Partition", func() { imgui.Checkbox("", &DBG.RenderSpatialPartition) })
+		imgui.EndTable()
 	}
 
 	if imgui.CollapsingHeaderV("RenderStats", imgui.TreeNodeFlagsNone) {
-		imgui.LabelText("Render Time", fmt.Sprintf("%f", DBG.RenderTime))
+		imgui.BeginTableV("Bloom Table", 2, imgui.TableFlagsBordersInnerV, imgui.Vec2{}, 0)
+		setupRow("Render Time", func() { imgui.LabelText("", fmt.Sprintf("%f", DBG.RenderTime)) })
+		imgui.EndTable()
+		var imageWidth float32 = 500
+		if DBG.DebugTexture != 0 {
+			texture := createUserSpaceTextureHandle(DBG.DebugTexture)
+			size := imgui.Vec2{X: imageWidth, Y: imageWidth / float32(renderContext.AspectRatio())}
+			// invert the Y axis since opengl vs texture coordinate systems differ
+			// https://learnopengl.com/Getting-started/Textures
+			imgui.ImageV(texture, size, imgui.Vec2{X: 0, Y: 1}, imgui.Vec2{X: 1, Y: 0}, imgui.Vec4{X: 1, Y: 1, Z: 1, W: 1}, imgui.Vec4{X: 0, Y: 0, Z: 0, W: 0})
+		}
 	}
 
-	var imageWidth float32 = 500
-	if DBG.DebugTexture != 0 {
-		texture := createUserSpaceTextureHandle(DBG.DebugTexture)
-		size := imgui.Vec2{X: imageWidth, Y: imageWidth / float32(renderContext.AspectRatio())}
-		// invert the Y axis since opengl vs texture coordinate systems differ
-		// https://learnopengl.com/Getting-started/Textures
-		imgui.ImageV(texture, size, imgui.Vec2{X: 0, Y: 1}, imgui.Vec2{X: 1, Y: 0}, imgui.Vec4{X: 1, Y: 1, Z: 1, W: 1}, imgui.Vec4{X: 0, Y: 0, Z: 0, W: 0})
-	}
 	imgui.End()
+}
+
+func setupRow(label string, item func()) {
+	imgui.TableNextRow()
+	imgui.TableNextColumn()
+	imgui.Text(label)
+	imgui.TableNextColumn()
+	imgui.PushItemWidth(300)
+	imgui.PushID(label)
+	item()
+	imgui.PopID()
+	imgui.PopItemWidth()
 }
 
 // some detailed comment here

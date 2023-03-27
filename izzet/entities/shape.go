2fddbfe82fbb5a81a8a54fe1b37425aa31737fd6
kkevinchou <kkevinchou@hotmail.com>
2023-03-27 16:33:50 -0400

    shape data

---
+++ izzet/entities/shape.go
@@ -0,0 +1,75 @@
+package entities
+
+import (
+	"github.com/go-gl/mathgl/mgl64"
+)
+
+type ShapeType string
+
+var (
+	CubeShapeType   ShapeType = "CUBE"
+	SphereShapeType ShapeType = "SPHERE"
+	LineShapeType   ShapeType = "LINE"
+)
+
+type CubeData struct {
+	Length int
+}
+
+type SphereData struct {
+	Radius float64
+}
+
+type CapsuleData struct {
+	Radius float64
+	Length float64
+}
+
+type LineData struct {
+	Vector mgl64.Vec3
+}
+
+type Triangle struct {
+	V1 mgl64.Vec3
+	V2 mgl64.Vec3
+	V3 mgl64.Vec3
+}
+
+type ShapeData struct {
+	Type     ShapeType
+	Cube     *CubeData
+	Sphere   *SphereData
+	Capsule  *CapsuleData
+	Line     *LineData
+	Triangle *Triangle
+}
+
+// take an int so that we don't explode the number of VAOs we create
+func CreateCube(length int) *Entity {
+	entity := InstantiateBaseEntity("cube", id)
+	entity.ShapeData = []*ShapeData{
+		&ShapeData{
+			Cube: &CubeData{
+				Length: length,
+			},
+		},
+	}
+	id += 1
+	return entity
+}
+
+func CreateTriangle(v1, v2, v3 mgl64.Vec3) *Entity {
+	entity := InstantiateBaseEntity("triangle", id)
+	entity.ShapeData = []*ShapeData{
+		&ShapeData{
+			Triangle: &Triangle{
+				V1: v1,
+				V2: v2,
+				V3: v3,
+			},
+		},
+	}
+	id += 1
+	return entity
+
+}

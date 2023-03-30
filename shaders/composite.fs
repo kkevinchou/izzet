b29d29858e6c3e9a3fc1d9516405a4b99ba58c25
kkevinchou <kkevinchou@hotmail.com>
2023-03-29 21:04:44 -0400

    tweaking colors

--- shaders/composite.fs
+++ shaders/composite.fs
@@ -42,6 +42,42 @@ vec3 acesToneMapping(vec3 color)
     return clamp(color, 0.0, 1.0);
 }
 
+const mat3 ACESInputMat = mat3
+(
+    0.59719, 0.35458, 0.04823,
+    0.07600, 0.90834, 0.01566,
+    0.02840, 0.13383, 0.83777
+);
+
+// ODT_SAT => XYZ => D60_2_D65 => sRGB
+const mat3 ACESOutputMat = mat3
+(
+     1.60475, -0.53108, -0.07367,
+    -0.10208,  1.10813, -0.00605,
+    -0.00327, -0.07276,  1.07602
+);
+
+vec3 RRTAndODTFit(vec3 v)
+{
+    vec3 a = v * (v + 0.0245786f) - 0.000090537f;
+    vec3 b = v * (0.983729f * v + 0.4329510f) + 0.238081f;
+    return a / b;
+}
+
+vec3 ACESFitted(vec3 color) {
+    color = ACESInputMat * color;
+
+    // Apply RRT and ODT
+    color = RRTAndODTFit(color);
+
+    color = ACESOutputMat * color;
+
+    // Clamp to [0, 1]
+    color = clamp(color, 0, 1);
+
+    return color;
+}
+
 void main()
 {
     // to bloom or not to bloom

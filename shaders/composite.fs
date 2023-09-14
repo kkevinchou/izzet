fa04fefbc04daf938f51a43ac8e328b8f82f6c69
kkevinchou <kkevinchou@hotmail.com>
2023-09-13 22:29:23 -0700

    shader cleanup

--- shaders/composite.fs
+++ shaders/composite.fs
@@ -7,22 +7,8 @@ uniform sampler2D scene;
 uniform sampler2D bloomBlur;
 uniform float exposure = 1.0f;
 uniform float bloomIntensity = 0.04f;
-// uniform int programChoice;
 
-vec3 bloom_none()
-{
-    vec3 hdrColor = texture(scene, TexCoords).rgb;
-    return hdrColor;
-}
-
-vec3 bloom_old()
-{
-    vec3 hdrColor = texture(scene, TexCoords).rgb;
-    vec3 bloomColor = texture(bloomBlur, TexCoords).rgb;
-    return hdrColor + bloomColor; // additive blending
-}
-
-vec3 bloom_new()
+vec3 compute_bloom_color()
 {
     vec3 hdrColor = texture(scene, TexCoords).rgb;
     vec3 bloomColor = texture(bloomBlur, TexCoords).rgb;
@@ -35,73 +21,19 @@ const float C = 2.43;
 const float D = 0.59;
 const float E = 0.14;
 
-// ACES tone mapping function
+
+// Narokowicz ACES tone mapping function
 vec3 acesToneMapping(vec3 color)
 {
     color = (color * (A * color + B)) / (color * (C * color + D) + E);
     return clamp(color, 0.0, 1.0);
 }
 
-const mat3 ACESInputMat = mat3
-(
-    0.59719, 0.35458, 0.04823,
-    0.07600, 0.90834, 0.01566,
-    0.02840, 0.13383, 0.83777
-);
-
-// ODT_SAT => XYZ => D60_2_D65 => sRGB
-const mat3 ACESOutputMat = mat3
-(
-     1.60475, -0.53108, -0.07367,
-    -0.10208,  1.10813, -0.00605,
-    -0.00327, -0.07276,  1.07602
-);
-
-vec3 RRTAndODTFit(vec3 v)
-{
-    vec3 a = v * (v + 0.0245786f) - 0.000090537f;
-    vec3 b = v * (0.983729f * v + 0.4329510f) + 0.238081f;
-    return a / b;
-}
-
-vec3 ACESFitted(vec3 color) {
-    color = ACESInputMat * color;
-
-    // Apply RRT and ODT
-    color = RRTAndODTFit(color);
-
-    color = ACESOutputMat * color;
-
-    // Clamp to [0, 1]
-    color = clamp(color, 0, 1);
-
-    return color;
-}
-
 void main()
 {
-    // to bloom or not to bloom
-    // vec3 result = vec3(0.0);
-    // switch (programChoice)
-    // {
-    // case 1: result = bloom_none(); break;
-    // case 2: result = bloom_old(); break;
-    // case 3: result = bloom_new(); break;
-    // default:
-    //     result = bloom_none(); break;
-    // }
-
-
-    // // tone mapping
-    // result = vec3(1.0) - exp(-result * exposure);
-    // // also gamma correct while we're at it
-    // const float gamma = 2.2;
-    // result = pow(result, vec3(1.0 / gamma));
-    // FragColor = vec4(result, 1.0);
-
-    vec3 color = bloom_new();
+    vec3 color = compute_bloom_color();
     color = acesToneMapping(color);
-    // color = color / (color + vec3(1.0));
+    // gamma correction
     color = pow(color, vec3(1.0 / 2.2));
 
     FragColor = vec4(color, 1.0);

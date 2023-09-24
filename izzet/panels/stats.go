83a8370c9ed471df59e94d612d489244915d3eaf
kkevinchou <kkevinchou@hotmail.com>
2023-09-23 17:51:40 -0700

    create a separate window for the texture viewer

--- izzet/panels/stats.go
+++ izzet/panels/stats.go
@@ -14,26 +14,35 @@ func stats(world World, renderContext RenderContext) {
 		setupRow("FPS", func() { imgui.LabelText("", fmt.Sprintf("%.1f", DBG.FPS)) })
 		setupRow("Triangle Draw Count", func() { imgui.LabelText("", formatNumber(DBG.TriangleDrawCount)) })
 		setupRow("Draw Count", func() { imgui.LabelText("", formatNumber(DBG.DrawCount)) })
-		setupRow("Texture", func() {
-			imgui.PushItemWidth(tableColumn1Width)
-			if imgui.BeginCombo("", string(SelectedComboOption)) {
-				for _, option := range comboOptions {
-					if imgui.Selectable(string(option)) {
-						SelectedComboOption = option
-					}
-				}
-				imgui.EndCombo()
-			}
-			imgui.PopItemWidth()
-		})
-		setupRow("Texture Viewer", func() {
+		setupRow("Texture Viewer Table Row", func() {
 			if DBG.DebugTexture != 0 {
-				var imageWidth float32 = 500
-				texture := createUserSpaceTextureHandle(DBG.DebugTexture)
-				size := imgui.Vec2{X: imageWidth, Y: imageWidth / float32(renderContext.AspectRatio())}
-				// invert the Y axis since opengl vs texture coordinate systems differ
-				// https://learnopengl.com/Getting-started/Textures
-				imgui.ImageV(texture, size, imgui.Vec2{X: 0, Y: 1}, imgui.Vec2{X: 1, Y: 0}, imgui.Vec4{X: 1, Y: 1, Z: 1, W: 1}, imgui.Vec4{X: 0, Y: 0, Z: 0, W: 0})
+				if imgui.Button("Toggle Texture Window") {
+					DBG.ShowDebugTexture = !DBG.ShowDebugTexture
+				}
+
+				if DBG.ShowDebugTexture {
+					imgui.SetNextWindowSizeV(imgui.Vec2{X: 400}, imgui.ConditionFirstUseEver)
+					if imgui.BeginV("Texture Viewer", &DBG.ShowDebugTexture, imgui.WindowFlagsNone) {
+						if imgui.BeginCombo("", string(SelectedComboOption)) {
+							for _, option := range comboOptions {
+								if imgui.Selectable(string(option)) {
+									SelectedComboOption = option
+								}
+							}
+							imgui.EndCombo()
+						}
+
+						regionSize := imgui.ContentRegionAvail()
+						imageWidth := regionSize.X
+
+						texture := createUserSpaceTextureHandle(DBG.DebugTexture)
+						size := imgui.Vec2{X: imageWidth, Y: imageWidth / float32(renderContext.AspectRatio())}
+						// invert the Y axis since opengl vs texture coordinate systems differ
+						// https://learnopengl.com/Getting-started/Textures
+						imgui.ImageV(texture, size, imgui.Vec2{X: 0, Y: 1}, imgui.Vec2{X: 1, Y: 0}, imgui.Vec4{X: 1, Y: 1, Z: 1, W: 1}, imgui.Vec4{X: 0, Y: 0, Z: 0, W: 0})
+					}
+					imgui.End()
+				}
 			}
 		})
 		imgui.EndTable()

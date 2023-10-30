1ce907d84e9f7e109082128b09bf51960a308ea7
kkevinchou <kkevinchou@hotmail.com>
2023-10-30 00:47:42 -0700

    cleanup

--- izzet/systems/serversystems/receiver.go
+++ izzet/systems/serversystems/receiver.go
@@ -20,10 +20,7 @@ func NewReceiverSystem(app App) *ReceiverSystem {
 
 func (s *ReceiverSystem) Update(delta time.Duration, world systems.GameWorld) {
 	for _, player := range s.app.GetPlayers() {
-		// for {
-		// processedMessage := false
 		select {
-
 		case message := <-player.InMessageChannel:
 			var inputMessage network.InputMessage
 			err := json.Unmarshal(message.Body, &inputMessage)
@@ -32,39 +29,9 @@ func (s *ReceiverSystem) Update(delta time.Duration, world systems.GameWorld) {
 				continue
 			}
 			s.app.InputBuffer().PushInput(message.CommandFrame, player.ID, inputMessage.Input)
-			// processedMessage = true
 		case <-player.DisconnectChannel:
 			world.QueueEvent(events.PlayerDisconnectEvent{PlayerID: player.ID})
 		default:
 		}
-
-		// if !processedMessage {
-		// 	break
-		// }
-		// }
 	}
-	// for {
-	// 	select {
-	// 	case message := <-s.app.NetworkMessagesChannel():
-	// 		if message.MessageType == network.MsgTypeGameStateUpdate {
-	// 			var gameStateUpdateMessage network.GameStateUpdateMessage
-	// 			err := json.Unmarshal(message.Body, &gameStateUpdateMessage)
-	// 			if err != nil {
-	// 				fmt.Println(fmt.Errorf("failed to deserialize message %w", err))
-	// 				continue
-	// 			}
-	// 			// fmt.Println(gameStateUpdateMessage)
-	// 		} else if message.MessageType == network.MsgTypePlayerInput {
-	// 			var inputMessage network.InputMessage
-	// 			err := json.Unmarshal(message.Body, &inputMessage)
-	// 			if err != nil {
-	// 				fmt.Println(fmt.Errorf("failed to deserialize message %w", err))
-	// 				continue
-	// 			}
-	// 			fmt.Println(inputMessage)
-	// 		}
-	// 	default:
-	// 		return
-	// 	}
-	// }
 }

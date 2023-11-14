e6f496e03ef6e3ea2f9eab17d812b48a983c07c3
kkevinchou <kkevinchou@hotmail.com>
2023-11-13 18:01:46 -0800

    refactor menu code

---
+++ izzet/render/menus/view.go
@@ -0,0 +1,26 @@
+package menus
+
+import (
+	"github.com/inkyblackness/imgui-go/v4"
+	"github.com/kkevinchou/izzet/izzet/render/renderiface"
+)
+
+func view(app renderiface.App) {
+	runtimeConfig := app.RuntimeConfig()
+	imgui.SetNextWindowSize(imgui.Vec2{X: 200})
+	if imgui.BeginMenu("View") {
+		if imgui.MenuItemV("Show Colliders", "", runtimeConfig.RenderColliders, true) {
+			runtimeConfig.RenderColliders = !runtimeConfig.RenderColliders
+		}
+
+		if imgui.MenuItemV("Show UI", "", runtimeConfig.UIEnabled, true) {
+			runtimeConfig.UIEnabled = !runtimeConfig.UIEnabled
+		}
+
+		if imgui.MenuItemV("ShowImguiDemo", "", app.ShowImguiDemo(), true) {
+			app.SetShowImguiDemo(!app.ShowImguiDemo())
+		}
+
+		imgui.EndMenu()
+	}
+}

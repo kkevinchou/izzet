04024525ec5781b729428b62dbc760ccbc963a18
kkevinchou <kkevinchou@hotmail.com>
2023-11-15 19:06:34 -0800

    add initialize file

---
+++ izzet/serialization/initialize.go
@@ -0,0 +1,27 @@
+package serialization
+
+import (
+	"github.com/kkevinchou/izzet/izzet/entities"
+	"github.com/kkevinchou/izzet/izzet/modellibrary"
+	"github.com/kkevinchou/kitolib/collision/collider"
+)
+
+func InitDeserializedEntity(entity *entities.Entity, ml *modellibrary.ModelLibrary) {
+	// set dirty flags
+	entity.DirtyTransformFlag = true
+
+	// rebuild animation player
+	if entity.Animation != nil {
+		handle := entity.Animation.AnimationHandle
+		entity.Animation = entities.NewAnimationComponent(handle, ml)
+	}
+
+	// rebuild trimesh collider
+	if entity.MeshComponent != nil {
+		meshHandle := entity.MeshComponent.MeshHandle
+		primitives := ml.GetPrimitives(meshHandle)
+		if len(primitives) > 0 {
+			entity.Collider.TriMeshCollider = collider.CreateTriMeshFromPrimitives(entities.MLPrimitivesTospecPrimitive(primitives))
+		}
+	}
+}

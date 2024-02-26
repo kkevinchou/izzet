33e07aaf583bba6d5a99b5be3fb7cc46e299fcf4
Kevin C <kkevinchou@hotmail.com>
2024-02-26 00:40:04 -0800

    vampires are cool

--- izzet/entities/ai.go
+++ izzet/entities/ai.go
@@ -1,4 +1,23 @@
 package entities
 
+import "github.com/go-gl/mathgl/mgl64"
+
 type AIComponent struct {
+	PatrolConfig   *PatrolConfig
+	RotationConfig *RotationConfig
+	TargetConfig   *TargetConfig
+	Speed          float64
+}
+
+type TargetConfig struct {
+	Direction mgl64.Vec3
+}
+
+type PatrolConfig struct {
+	Points []mgl64.Vec3
+	Index  int
+}
+
+type RotationConfig struct {
+	Quat mgl64.Quat
 }

6c343972aab0609fa8872809ce478de5c9782e99
Kevin C <kkevinchou@hotmail.com>
2024-02-25 20:27:35 -0800

    initial ai setup

--- izzet/systems/serversystems/spawner.go
+++ izzet/systems/serversystems/spawner.go
@@ -22,33 +22,39 @@ func NewSpawnerSystem(app App) *SpawnerSystem {
 }
 
 func (s *SpawnerSystem) Update(delta time.Duration, world systems.GameWorld) {
-	var radius float64 = 40
-	var length float64 = 80
+	// var radius float64 = 40
+	// var length float64 = 80
 
 	for _, player := range s.app.GetPlayers() {
 		frameInput := s.app.GetPlayerInput(player.ID)
 		if frameInput.KeyboardInput[input.KeyboardKeyJ].Event == input.KeyboardEventUp {
-			entity := entities.InstantiateEntity("some_dude")
+			handle := modellibrary.NewGlobalHandle("vampire")
+			entity := entities.InstantiateEntity("vampire")
 			entity.Physics = &entities.PhysicsComponent{GravityEnabled: true}
 			entity.Collider = &entities.ColliderComponent{
-				CapsuleCollider: &collider.Capsule{
-					Radius: radius,
-					Top:    mgl64.Vec3{0, radius + length, 0},
-					Bottom: mgl64.Vec3{0, radius, 0},
-				},
+				// CapsuleCollider: &collider.Capsule{
+				// 	Radius: radius,
+				// 	Top:    mgl64.Vec3{0, radius + length, 0},
+				// 	Bottom: mgl64.Vec3{0, radius, 0},
+				// },
 				ColliderGroup: entities.ColliderGroupFlagPlayer,
 				CollisionMask: entities.ColliderGroupFlagTerrain | entities.ColliderGroupFlagPlayer,
 			}
 
-			capsule := entity.Collider.CapsuleCollider
-			entity.InternalBoundingBox = collider.BoundingBox{MinVertex: capsule.Bottom.Sub(mgl64.Vec3{radius, radius, radius}), MaxVertex: capsule.Top.Add(mgl64.Vec3{radius, radius, radius})}
+			primitives := s.app.ModelLibrary().GetPrimitives(handle)
+			verts := modellibrary.UniqueVerticesFromPrimitives(primitives)
+			c := collider.NewCapsuleFromVertices(verts)
+			entity.Collider.CapsuleCollider = &c
 
-			handle := modellibrary.NewGlobalHandle("alpha2")
+			capsule := entity.Collider.CapsuleCollider
+			entity.InternalBoundingBox = collider.BoundingBox{MinVertex: capsule.Bottom.Sub(mgl64.Vec3{c.Radius, c.Radius, c.Radius}), MaxVertex: capsule.Top.Add(mgl64.Vec3{c.Radius, c.Radius, c.Radius})}
+
 			entity.MeshComponent = &entities.MeshComponent{MeshHandle: handle, Transform: mgl64.Rotate3DY(180 * math.Pi / 180).Mat4(), Visible: true, ShadowCasting: true}
-			entity.Animation = entities.NewAnimationComponent("alpha2", s.app.ModelLibrary())
-			entities.SetScale(entity, mgl64.Vec3{0.25, 0.25, 0.25})
+			entity.Animation = entities.NewAnimationComponent("vampire", s.app.ModelLibrary())
+			entity.AIComponent = &entities.AIComponent{}
 			entities.SetLocalPosition(entity, mgl64.Vec3{0, 10, 0})
 			entity.Movement = &entities.MovementComponent{PatrolConfig: &entities.PatrolConfig{Points: []mgl64.Vec3{{0, 10, 0}, {-300, 10, 0}}}, Speed: 100}
+
 			world.QueueEvent(events.EntitySpawnEvent{Entity: entity})
 		}
 	}

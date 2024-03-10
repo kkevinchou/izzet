9959c74d233f40e1babffcde16f179d67cf86aa7
Kevin C <kkevinchou@hotmail.com>
2024-03-10 01:20:09 -0800

    neighbor construction

--- izzet/navmesh/navmesh.go
+++ izzet/navmesh/navmesh.go
@@ -8,9 +8,30 @@ import (
 
 const maxHeight int = 0xffff
 
+// match what recast uses, they also reserve 63 as unconnected but we don't use that
+const maxLayers int = 62
+
+var dirs [4]int = [4]int{0, 1, 2, 3}
+var xDirs [4]int = [4]int{-1, 0, 1, 0}
+var zDirs [4]int = [4]int{0, 1, 0, -1}
+
 type NavigationMesh struct {
 	HeightField *HeightField
 	Volume      collider.BoundingBox
+}
+
+func Min[T cmp.Ordered](a T, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max[T cmp.Ordered](a T, b T) T {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func Clamp[T cmp.Ordered](value, minInclusive, maxInclusive T) T {
@@ -18,6 +39,13 @@ func Clamp[T cmp.Ordered](value, minInclusive, maxInclusive T) T {
 		return minInclusive
 	} else if value > maxInclusive {
 		return maxInclusive
+	}
+	return value
+}
+
+func Abs(value int) int {
+	if value < 0 {
+		return -value
 	}
 	return value
 }

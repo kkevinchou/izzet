c298187f797332a9550ee27f8701703dc0e07bc5
Kevin C <kkevinchou@hotmail.com>
2024-06-27 21:13:49 -0700

    create a WIP material, and preview the texture

--- izzet/render/panels/drawer/materials.go
+++ izzet/render/panels/drawer/materials.go
@@ -2,20 +2,25 @@ package drawer
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	imgui "github.com/AllenDang/cimgui-go"
+	"github.com/kkevinchou/izzet/izzet/apputils"
 	"github.com/kkevinchou/izzet/izzet/material"
 	"github.com/kkevinchou/izzet/izzet/prefabs"
 	"github.com/kkevinchou/izzet/izzet/render/panels/panelutils"
 	"github.com/kkevinchou/izzet/izzet/render/renderiface"
-	"github.com/kkevinchou/izzet/izzet/types"
+	"github.com/sqweek/dialog"
 )
 
 var (
-	tableFlags imgui.TableFlags = imgui.TableFlagsBordersInnerV
+	tableFlags  imgui.TableFlags = imgui.TableFlagsBordersInnerV
+	WIPMaterial material.Material
 )
 
 func materialssUI(app renderiface.App, ps []*prefabs.Prefab) bool {
+	mat := &WIPMaterial
 	var menuOpen bool
 	if imgui.BeginTabItem("Materials") {
 		if imgui.CollapsingHeaderTreeNodeFlagsV("Materials", imgui.TreeNodeFlagsDefaultOpen) {
@@ -23,28 +28,56 @@ func materialssUI(app renderiface.App, ps []*prefabs.Prefab) bool {
 			panelutils.InitColumns()
 			// imgui.Tablepanelutils.SetupColumnV("0", imgui.TableColumnFlagsWidthFixed, tableColumn0Width, 0)
 
-			material := material.Material{
-				PBR: types.PBR{},
-			}
-
 			// Frame Profiling
 			panelutils.SetupRow("Diffuse", func() {
-				imgui.ColorEdit3V("", &material.PBR.Diffuse, imgui.ColorEditFlagsNoInputs|imgui.ColorEditFlagsNoLabel)
+				imgui.ColorEdit3V("", &mat.PBR.Diffuse, imgui.ColorEditFlagsNoInputs|imgui.ColorEditFlagsNoLabel)
 			}, true)
 			panelutils.SetupRow("Invisible", func() {
-				imgui.Checkbox("", &material.Invisible)
+				imgui.Checkbox("", &mat.Invisible)
 			}, true)
 
 			panelutils.SetupRow("Diffuse Intensity", func() {
-				imgui.SliderFloatV("", &material.PBR.DiffuseIntensity, 1, 20, "%.1f", imgui.SliderFlagsNone)
+				imgui.SliderFloatV("", &mat.PBR.DiffuseIntensity, 1, 20, "%.1f", imgui.SliderFlagsNone)
 			}, true)
 
-			panelutils.SetupRow("Roughness", func() { imgui.SliderFloatV("", &material.PBR.Roughness, 0, 1, "%.2f", imgui.SliderFlagsNone) }, true)
-			panelutils.SetupRow("Metallic Factor", func() { imgui.SliderFloatV("", &material.PBR.Metallic, 0, 1, "%.2f", imgui.SliderFlagsNone) }, true)
+			panelutils.SetupRow("Roughness", func() { imgui.SliderFloatV("", &mat.PBR.Roughness, 0, 1, "%.2f", imgui.SliderFlagsNone) }, true)
+			panelutils.SetupRow("Metallic Factor", func() { imgui.SliderFloatV("", &mat.PBR.Metallic, 0, 1, "%.2f", imgui.SliderFlagsNone) }, true)
+			panelutils.SetupRow("Texture", func() { imgui.LabelText("", mat.PBR.TextureName) }, true)
+
+			if mat.PBR.TextureName != "" {
+				t := app.AssetManager().GetTexture(mat.PBR.TextureName)
+				texture := imgui.TextureID{Data: uintptr(t.ID)}
+				size := imgui.Vec2{X: 50, Y: 50}
+				imgui.ImageV(texture, size, imgui.Vec2{X: 0, Y: 1}, imgui.Vec2{X: 1, Y: 0}, imgui.Vec4{X: 1, Y: 1, Z: 1, W: 1}, imgui.Vec4{X: 0, Y: 0, Z: 0, W: 0})
+			}
+
+			if imgui.Button("Import Texture") {
+				// loading the asset
+				d := dialog.File()
+				currentDir, err := os.Getwd()
+				if err != nil {
+					panic(err)
+				}
+				d = d.SetStartDir(filepath.Join(currentDir, "_assets"))
+				d = d.Filter("PNG file", "png")
+
+				assetFilePath, err := d.Load()
+				if err != nil {
+					if err != dialog.ErrCancelled {
+						panic(err)
+					}
+				} else {
+					i := 0
+					baseFileName := apputils.NameFromAssetFilePath(assetFilePath)
+					mat.PBR.TextureName = baseFileName
+					mat.PBR.ColorTextureIndex = &i
+				}
+			}
 
 			imgui.EndTable()
 			if imgui.Button("Create Material") {
-				app.CreateMaterial(material)
+				app.CreateMaterial(*mat)
+				WIPMaterial = material.Material{}
 			}
 		}
 		imgui.EndTabItem()

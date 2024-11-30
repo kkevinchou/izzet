c23410af1ed362734ef7bd625a3d74ac2b0c73b5
Kevin C <kkevinchou@hotmail.com>
2024-11-29 22:31:39 -0800

    cap fps to 60

--- izzet/settings/settings.go
+++ izzet/settings/settings.go
@@ -32,11 +32,11 @@ const (
 	MSPerGameStateUpdate int = 100
 
 	// FPS is the number of rendered frames per second, separate from command frames
-	FPS int = 144
+	FPS int = 60
 
 	// MSPerCommandFrame is the size of the simulation step for reading input,
 	// physics, etc.
-	MSPerCommandFrame int = 8
+	MSPerCommandFrame int = 16
 
 	// the maximum number of command frames to execute in a single loop to prevent the spiral of death
 	MaxCommandFramesPerLoop int = 3
